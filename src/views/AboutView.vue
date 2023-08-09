810311ed7de6b349c4a1ee47728d2d2ad03789e2
Young <628063@bah.com>
2023-08-09 15:13:12 -0400

    First commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>A Site for Events to Better the World</h1>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/views/EventListView.vue
@@ -0,0 +1,51 @@
+<script setup>
+import EventCard from '../components/EventCard.vue'
+import  { ref } from 'vue'
+
+const events = ref([
+{
+      id: 123,
+      category: "animal welfare",
+      title: "Cat Adoption Day",
+      description: "Find your new feline friend at this event.",
+      location: "Meow Town",
+      date: "January 28, 2022",
+      time: "12:00",
+      organizer: "Kat Laydee"
+    },
+    {
+      id: 456,
+      category: "food",
+      title: "Community Gardening",
+      description: "Join us as we tend to the community edible plants.",
+      location: "Flora City",
+      date: "March 14, 2022",
+      time: "10:00",
+      organizer: "Fern Pollin"
+    },
+    {
+      id: 789,
+      category: "sustainability",
+      title: "Beach Cleanup",
+      description: "Help pick up trash along the shore.",
+      location: "Playa Del Carmen",
+      date: "July 22, 2022",
+      time: "11:00",
+      organizer: "Carey Wales"
+    }
+])
+</script>
+
+<template>
+  <h1>Events for Good</h1>
+  <div class="events"></div>
+  <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
